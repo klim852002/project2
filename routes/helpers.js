36b89bd9d6b716ed74d521c2342f387410a508d7
KUOPING LEE <blu_blac@hotmail.com>
2016-10-30 22:39:30 +0800

    commented files and removed flash error from helperlist

--- routes/helpers.js
+++ routes/helpers.js
@@ -4,7 +4,8 @@ var passport = require('passport')
 
 var Helper = require('../models/helper')
 var Task = require('../models/task')
-function authCheck (req, res, next) {
+
+function authCheck(req, res, next) {
   // if req.isAuthenticated is false, then let it be
 
   // if it's true, redirect back to profile
@@ -16,54 +17,58 @@ function authCheck (req, res, next) {
   }
 }
 
-function isLoggedIn (req, res, next) {
+function isLoggedIn(req, res, next) {
   // if user is authenticated in the session, carry on
   if (req.isAuthenticated())
     return next()
-  // otherwise redirect them to the home page
+      // otherwise redirect them to the home page
   res.redirect('/')
 }
 // using the local passport strategy for signup
 router.route('/signup')
-      .get(authCheck, function (req, res) {
-        Helper.find({}, function (err, allHelpers) {
-          res.render('helpers/signup', {
-            allHelpers: allHelpers,
-            message: req.flash('signupMessage')
-          })
-        })
+  .get(authCheck, function(req, res) {
+    Helper.find({}, function(err, allHelpers) {
+      res.render('helpers/signup', {
+        allHelpers: allHelpers,
+        message: req.flash('signupMessage')
       })
-      .post(passport.authenticate('helper-signup', {
-        successRedirect: '/helpers/profile',
-        failureRedirect: '/helpers/signup',
-        failureFlash: true
-      }))
+    })
+  })
+  .post(passport.authenticate('helper-signup', {
+    successRedirect: '/helpers/profile',
+    failureRedirect: '/helpers/signup',
+    failureFlash: true
+  }))
 // using the local passport strategy for signup
 
 // local passport strategy for authenticating login
 router.route('/login')
-      .get(function (req, res) {
-        res.render('helpers/login', { message: req.flash('loginMessage') })
-      })
-      .post(passport.authenticate('helper-login', {
-        successRedirect: '/helpers/profile',
-        failureRedirect: '/helpers/login',
-        failureFlash: true
-      }))
-// local passport strategy for authenticating login
-
-router.get('/profile', isLoggedIn, function (req, res) {
-  Task.find({helper: req.user.id}).populate('tasker').exec(function (err, taskList) {
-    if (err) console.log(err)
-    res.render('helpers/profile', {
-      taskList: taskList,
-      user: req.user.local.name
+  .get(function(req, res) {
+    res.render('helpers/login', {
+      message: req.flash('loginMessage')
     })
   })
-})
-// , { message: req.flash('loginMessage') }
+  .post(passport.authenticate('helper-login', {
+    successRedirect: '/helpers/profile',
+    failureRedirect: '/helpers/login',
+    failureFlash: true
+  }))
+  // local passport strategy for authenticating login
 
-router.get('/logout', function (req, res) {
+router.get('/profile', isLoggedIn, function(req, res) {
+    Task.find({
+      helper: req.user.id
+    }).populate('tasker').exec(function(err, taskList) {
+      if (err) console.log(err)
+      res.render('helpers/profile', {
+        taskList: taskList,
+        user: req.user.local.name
+      })
+    })
+  })
+  // , { message: req.flash('loginMessage') }
+
+router.get('/logout', function(req, res) {
   req.logout()
   res.redirect('/')
 })
@@ -100,59 +105,5 @@ router.get('/logout', function (req, res) {
 // setting the route to homepage
 // app.get('/path-name', callback(request, response)) NO
 // use router.get instead
-
-// READ ROUTES
-
-// // All the GET requests
-// router.get('/', function (req, res) {
-//   // all users view under INDEX.EJS
-//   res.render('users/index')
-// }).get('/new', function (req, res) {
-//   // NEW route under NEW.EJS
-//   res.render('users/new')
-// }).get('/:id', function (req, res) {
-//   res.send('ROUTES GOES HERE INSTEAD')
-// }).get('/:id/edit', function (req, res) {
-//  // {
-//  //  name: 'glen',
-//  //  password: 'glen123'
-//  // }
-//
-//  // connect to the database
-//  // retrieve it by the id
-//  // store it in a variable
-//  // pass it over to the view
-//   var userdata = {
-//     name: 'glen chooooooo',
-//     password: 'glen123'
-//   }
-//
-//   res.render('users/edit', userdata)
-//   // res.send('edit user\'s ' + req.params.id + ' details')
-// })
-//
-// // the only POST request
-// router.post('/', function (req, res) {
-//   // get the post parameter
-//   // {
-//   // username: "primaulia",
-//   // userpassword: "test123"
-//   // }
-//   // var posted_username = req.body.username
-//   // var posted_password = req.body.userpassword
-//   res.send(req.body)
-//   // res.send('posted username is ' + posted_username + ' and posted password is: ' + posted_password)
-// })
-//
-// // only PUT request
-// router.put('/:id', function (req, res) {
-//   res.send('SHOULDVE GONE HERE')
-// })
-//
-// // DELETE ROUTES
-// router.delete('/:id', function (req, res) {
-//   res.send('delete user' + req.params.id)
-// })
-
 // ===
 module.exports = router
--- routes/tasks.js
+++ routes/tasks.js
@@ -7,7 +7,7 @@ var Tasker = require('../models/tasker')
 var Helper = require('../models/helper')
 
 
-function authCheck (req, res, next) {
+function authCheck(req, res, next) {
   // if req.isAuthenticated is false, then let it be
 
   // if it's true, redirect back to profile
@@ -19,103 +19,106 @@ function authCheck (req, res, next) {
   }
 }
 
-function isLoggedIn (req, res, next) {
+function isLoggedIn(req, res, next) {
   // if user is authenticated in the session, carry on
   if (req.isAuthenticated())
     return next()
-  // otherwise redirect them to the home page
+      // otherwise redirect them to the home page
   req.flash('remindLoginMessage', 'Please login to access the page')
   res.redirect('/')
   message: req.flash('remindLoginMessage')
 }
 
 // get request to render form for new task
-router.get('/newtask', isLoggedIn, function (req, res) {
-  // res.send(req.user)
-  res.render('tasks/newtask')
-})
-// post request to create new task
-router.post('/newtask', isLoggedIn, function (req, res) {
-  // Task.create(req.body.task, function
-  console.log(req.user)
-  var newTask = new Task({
-    title: req.body.task.title,
-    description: req.body.task.description,
-    date: req.body.task.date,
-    time: req.body.task.time,
-    location: req.body.task.location,
-    tasker: req.user.id
+router.get('/newtask', isLoggedIn, function(req, res) {
+    // res.send(req.user)
+    res.render('tasks/newtask')
   })
-  newTask.save(function (err, task) {
-    if (err) {
-      res.send('an err during creation' + err)
-    } else {
-      // res.redirect('/profile')
-      // res.send('successful')
-      res.redirect('/taskers/profile')
-    }
+  // post request to create new task
+router.post('/newtask', isLoggedIn, function(req, res) {
+    // Task.create(req.body.task, function
+    console.log(req.user)
+    var newTask = new Task({
+      title: req.body.task.title,
+      description: req.body.task.description,
+      date: req.body.task.date,
+      time: req.body.task.time,
+      location: req.body.task.location,
+      tasker: req.user.id
+    })
+    newTask.save(function(err, task) {
+      if (err) {
+        res.send('an err during creation' + err)
+      } else {
+        // res.redirect('/profile')
+        // res.send('successful')
+        res.redirect('/taskers/profile')
+      }
+    })
   })
-})
-// get request to render all Task listings
-router.get('/listings', function (req, res) {
-  Task.find({}, function (err, allTasks) {
+  // get request to render all Task listings
+router.get('/listings', function(req, res) {
+  Task.find({}, function(err, allTasks) {
     console.log(allTasks)
     res.render('tasks/listings', {
       allTasks: allTasks
     })
   })
 })
-
-
-
-router.get('/helperlist', isLoggedIn, function (req, res) {
-  // if (!isLoggedIn) {
-  //   rmessage: req.flash('remindLoginMessage'),
-  // } else {
-  Task.find({message: null}, function (err, allTasks) {
-    console.log(allTasks)
-    res.render('tasks/helperlist', {
-      message: req.flash('remindLoginMessage'),
-      allTasks: allTasks
+// get request for helperlist  (open tasks that are viewed by logged in helper)
+router.get('/helperlist', isLoggedIn, function(req, res) {
+  if (!isLoggedIn) {
+    message: req.flash('remindLoginMessage')
+  }
+  else {
+    Task.find({
+      message: null
+    }, function(err, allTasks) {
+      console.log(allTasks)
+      res.render('tasks/helperlist', {
+        message: req.flash('remindLoginMessage'),
+        allTasks: allTasks
+      })
+    })
+  }
+})
+// get request to view individual task
+router.get('/:id/message', isLoggedIn, function(req, res) {
+  Task.findOne({
+    _id: req.params.id
+  }, function(err, viewTask) {
+    res.render('tasks/message', {
+      viewTask: viewTask
     })
   })
 })
-
-router.get('/:id/message', isLoggedIn, function (req, res) {
-  Task.findOne({_id: req.params.id}, function(err, viewTask) {
-      res.render('tasks/message', {
-          viewTask: viewTask
-      })
-  })
-})
-
+// post request to insert message to tasker
 router.post('/:id/message', isLoggedIn, function (req, res) {
-  Task.findOne ({_id: req.params.id}, function (err, leaveMsg)
-  {
-    if (err) {
-      res.send('cannot leave message')
-    } else {
+    Task.findOne({_id: req.params.id}, function (err, leaveMsg) {
+      if (err) {
+        res.send('cannot leave message')
+      } else {
         // console.log(user)
-        leaveMsg.helper=req.user.id
-        leaveMsg.message=req.body.task.message
+        leaveMsg.helper = req.user.id
+        leaveMsg.message = req.body.task.message
         leaveMsg.save(function (err, leaveMsg) {
           res.redirect('/helpers/profile')
         })
       }
+    })
   })
-})
-// below is stable for individual task comment.
-// router.get('/:id/message', function (req, res) {
-//   Task.findOne({_id: req.params.id}, function(err, viewTask) {
-//     if (err) {
-//       console.log(err)
-//     } else {
-//       // res.render('tasks/message')
-//       // viewTask: viewTask
-//       res.send('hi')
-//     }
-//   })
-// })
+  // below is stable for individual task comment.
+  // router.get('/:id/message', function (req, res) {
+  //   Task.findOne({_id: req.params.id}, function(err, viewTask) {
+  //     if (err) {
+  //       console.log(err)
+  //     } else {
+  //       // res.render('tasks/message')
+  //       // viewTask: viewTask
+  //       res.send('hi')
+  //     }
+  //   })
+  // })
 
 // router.get('/profile/:id/edit', function (req, res) {
 //   Task.findOne({_id: req.params.id}, function(err, selectedTask) {
